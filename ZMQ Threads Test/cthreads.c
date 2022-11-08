30c5ebd99613464c1866e52dda6ab1d85a9da56d
Ethan Mayer <ethan.mayer@comcast.net>
2022-11-08 00:22:58 -0600

    Thread launches with function pointer as body from shared library

--- ZMQ Threads Test/cthreads.c
+++ ZMQ Threads Test/cthreads.c
@@ -6,6 +6,7 @@
 #include "cthreads.h"
 #include <zmq.h>
 #include <unistd.h>
+#include <dlfcn.h>
 //#include <czmq.h>
 
 // error handling
@@ -15,43 +16,43 @@ void error(char* msg) {
     exit(1);
 }
 
-void* thread1() {
-    long thread = pthread_self();
-    printf("Thread %ld started\n", thread);
-    fflush(stdout);
+// void* thread1() {
+//     long thread = pthread_self();
+//     printf("Thread %ld started\n", thread);
+//     fflush(stdout);
 
-    void *context = zmq_ctx_new();
-    void *subscriber = zmq_socket(context, ZMQ_SUB);
-    if (zmq_connect(subscriber, "tcp://127.0.0.1:5556") != 0) {
-        error("Could not connect to pub socket\n");
-    }
-    if (zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0) != 0) {
-        error("Could not set sock opt for sub socket\n");
-    }
+//     void *context = zmq_ctx_new();
+//     void *subscriber = zmq_socket(context, ZMQ_SUB);
+//     if (zmq_connect(subscriber, "tcp://127.0.0.1:5556") != 0) {
+//         error("Could not connect to pub socket\n");
+//     }
+//     if (zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0) != 0) {
+//         error("Could not set sock opt for sub socket\n");
+//     }
 
-    int arr[3];
+//     int arr[3];
 
-    for (int i = 0; i < 100; i++)
-    {
-        sleep(5);
-        if (zmq_recv(subscriber, arr, sizeof(arr), 0) == -1) {
-            error("Could not receive on pub socket\n");
-        }
-        printf("Thread %ld: Received array of size %d: ", thread, sizeof(arr)/sizeof(arr[0]));
-        for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-        fflush(stdout);
-    }
+//     for (int i = 0; i < 100; i++)
+//     {
+//         sleep(5);
+//         if (zmq_recv(subscriber, arr, sizeof(arr), 0) == -1) {
+//             error("Could not receive on pub socket\n");
+//         }
+//         printf("Thread %ld: Received array of size %d: ", thread, sizeof(arr)/sizeof(arr[0]));
+//         for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+//             printf("%d ", arr[i]);
+//         }
+//         printf("\n");
+//         fflush(stdout);
+//     }
 
-    zmq_close(subscriber);
-    zmq_ctx_destroy(context);
+//     zmq_close(subscriber);
+//     zmq_ctx_destroy(context);
 
-    printf("Thread %ld exiting\n", thread);
-    fflush(stdout);
-    return NULL;
-}
+//     printf("Thread %ld exiting\n", thread);
+//     fflush(stdout);
+//     return NULL;
+// }
 
 // void* thread2() {
 
@@ -71,7 +72,16 @@ int start_test(int arr[], int arrSize) {
         error("Could not bind pub socket\n");
     }
 
-    if (pthread_create(&t1, NULL, &thread1, NULL) == -1) {
+    char* libpath = "funcBody.so";
+    void* libhandle = dlopen(libpath, RTLD_LAZY);
+
+    if (libhandle == NULL) {
+        error("Could not open shared library");
+    }
+
+    void (*thread1) = dlsym(libhandle, "thread1");
+
+    if (pthread_create(&t1, NULL, thread1, NULL) == -1) {
         error("Can't create thread\n");
     }
     // if (pthread_create(&t2, NULL, &thread2, NULL) == -1) {
