ce17a757d900cd9a30f2bbcfeb642d8ed4e63846
Ethan Mayer <ethan.mayer@comcast.net>
2022-11-21 22:47:46 -0600

    Minor renames and code cleanup

--- Actor/setup.py
+++ Actor/setup.py
@@ -4,7 +4,7 @@ from Cython.Distutils import build_ext
 # first arg in Extension is the name of the produced module
 # second arg, the list, is the cython file and the c file
 # third is the numpy dir as it will be used inside the cython code
-ext_modules = [Extension("zmq_threads", ["pyxthreads.pyx", "cthreads.c"],
+ext_modules = [Extension("comp", ["comp.pyx", "cthreads.c"],
                          include_dirs = ["/opt/homebrew/Cellar/zeromq/4.3.4/include"],
                          library_dirs = ["/opt/homebrew/Cellar/zeromq/4.3.4/lib"],
                          libraries = ["zmq"])]
