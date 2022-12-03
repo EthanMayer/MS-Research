3cd18f448383c3eaa1a779e464e6be7cf9ac7189
Ethan Mayer <ethan.mayer@comcast.net>
2022-12-03 13:12:09 -0600

    Cython version of zmq implemented

--- Actor/comp.pyx
+++ Actor/comp.pyx
@@ -8,12 +8,9 @@ from libc.stdlib cimport malloc, free
 from posix.dlfcn cimport dlopen, dlsym, RTLD_LAZY
 from libc.string cimport strerror
 from libc.errno cimport errno
+from libc.stdio cimport sprintf
 from pthread cimport pthread_create, pthread_join, pthread_t
-cimport zmq
-
-# Import the interface function declaration from the header file
-cdef extern from "cthreads.h":
-    int* start_test(int arr[], int arrSize)
+from zmq.backend.cython cimport libzmq as z
     
 # Error handling
 cpdef error(msg):
@@ -35,10 +32,10 @@ cpdef tuple main(tup):
         arr[i] = tup[i]
 
     # Create main thread pair socket
-    cdef void* context = zmq_ctx_new()
-    cdef void* sckt = zmq_socket(context, ZMQ_PAIR)
+    cdef void* context = z.zmq_ctx_new()
+    cdef void* sckt = z.zmq_socket(context, z.ZMQ_PAIR)
 
-    if (zmq_bind(sckt, "tcp://127.0.0.1:5556") != 0):
+    if (z.zmq_bind(sckt, "tcp://127.0.0.1:5556") != 0):
         error("Could not bind main receiver socket")
 
     # Create entry for thread1's socket and add it to dictionary
@@ -59,53 +56,42 @@ cpdef tuple main(tup):
 
     # Receive "Ready" message to know the thread is ready
     cdef char buf[6]
-    if (zmq_recv(sckt, buf, sizeof(buf), 0) == -1):
+    if (z.zmq_recvbuf(sckt, buf, sizeof(buf), 0) == -1):
         error("Could not receive on main receive socket")
     print("Main received: " + str(buf))
 
     # Send array size via socket to threads
     cdef char sizeBuf[256]
     sprintf(sizeBuf, "%d", n)
-    if (zmq_send(sckt, sizeBuf, sizeof(sizeBuf), 0) != sizeof(sizeBuf)):
+    if (z.zmq_sendbuf(sckt, sizeBuf, sizeof(sizeBuf), 0) != sizeof(sizeBuf)):
         error("Pair send buffer length incorrect\n")
     print("Main: Sent array size")
     # socket.send_pyobj(n)
 
     # Send array via socket to thread
-    if (zmq_send(sckt, arr, arrSize * sizeof(int), 0) != arrSize * sizeof(int)):
+    if (z.zmq_sendbuf(sckt, arr, n * sizeof(int), 0) != n * sizeof(int)):
         error("Pair send buffer length incorrect\n")
     print("Main: Sent array")
 
     # Receive array back via socket from thread
-    if (zmq_recv(sckt, arr2, arrSize * sizeof(int), 0) == -1):
+    if (z.zmq_recvbuf(sckt, arr2, n * sizeof(int), 0) == -1):
         error("Could not receive on main receive socket")
+    
+    # Convert received array to Python tuple
+    tup2 = []
     for i in range(n):
         arr2[i] = tup2[i]
+    free(arr2)
 
     # Clean up socket
-    zmq_close(sckt)
-    zmq_ctx_destroy(context)
+    z.zmq_close(sckt)
+    z.zmq_ctx_destroy(context)
 
     # Join thread
     print("Main: Joining thread\n")
     if (pthread_join(t1, NULL) == -1):
         error("Can't join thread 1")
 
-    # Call C file
-    #print("Sending tuple to C as C array")
-    #try:
-    #    arr2 = start_test(arr, n)
-    #finally:
-    #    free(arr)
-    
-    # Convert C array to tuple
-    #tup2 = []
-    #for i in range(n):
-    #    tup2.append(int(arr2[i]))
-    #tup2 = tuple(tup2)
-    #free(arr2)
-
     # Return tuple to Python
-    print("======== Comp.pyx ========")
     print("Sending C array to Python as tuple")
     return tup2
