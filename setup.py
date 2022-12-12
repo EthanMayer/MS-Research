88d712135ff1df99bee866425b638096eb939051
Ethan Mayer <ethan.mayer@comcast.net>
2022-12-12 15:49:21 -0600

    Removed uneeded dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Distutils import build_ext
 # first arg in Extension is the name of the produced module
 # second arg, the list, is the cython file and the c file
 # third is the numpy dir as it will be used inside the cython code
-ext_modules = [Extension("comp", ["comp.pyx", "cthreads.c"],
+ext_modules = [Extension("comp", ["comp.pyx"],
                          include_dirs = ["/opt/homebrew/Cellar/zeromq/4.3.4/include", "/opt/homebrew/lib/python3.9/site-packages/zmq/utils"],
                          library_dirs = ["/opt/homebrew/Cellar/zeromq/4.3.4/lib"],
                          libraries = ["zmq"])]
